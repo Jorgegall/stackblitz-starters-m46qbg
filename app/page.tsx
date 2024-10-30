281bacfaa2bc0757cf359bd1d6ef5f04211431ce
Jorgegall <git@stackblitz.com>
2024-10-30 14:43:55 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+// data/events.json
+[
+  {
+    "id": 1,
+    "title": "Event Solana I",
+    "date": "2024-11-01",
+    "location": "Montreal, Canada",
+    "description": "Solana meet #1."
+  },
+  {
+    "id": 2,
+    "title": "Event Solana",
+    "date": "2024-12-15",
+    "location": "Montreal, Canada",
+    "description": "Solana meet #2."
+  }
+]
